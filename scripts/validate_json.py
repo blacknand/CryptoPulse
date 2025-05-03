72953a4c6e9c54ae6df01d80b1ff2281bb60f6ac
Nathan Blackburn <nblackburndeveloper@icloud.com>
2025-05-03 18:29:20 +0100

    add colours

--- scripts/validate_json.py
+++ scripts/validate_json.py
@@ -1,13 +1,14 @@
+from colorama import Fore, Back, Style
 import json
 import os
 
-print(f"CWD: {os.getcwd()}")
+print(f"{Fore.GREEN}[INFO]{Style.RESET_ALL} CWD: {os.getcwd()}")
 
 try:
     with open('/user_data/backtest_results/backtest-result.json', 'r') as f:
         data = json.load(f)
     print(data.keys())
 except FileNotFoundError:
-    print("File does not exist")
+    print(f"{Fore.RED}[ERROR]{Style.RESET_ALL} file does not exist")
 except:
-    print("Unexpected error")
+    print(f"{Fore.RED}[ERROR]{Style.RESET_ALL} unexpected error has occured")
